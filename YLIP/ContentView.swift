a6454a28d85711afd3e661683a2b50bc81e1d985
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2024-04-08 20:55:16 -0700

    added util.swift
    a bit better code organization

--- YLIP/ContentView.swift
+++ YLIP/ContentView.swift
@@ -1,75 +1,34 @@
 import SwiftUI
 
-struct ContentView: SwiftUI.View {
+struct ContentView: View {
     @State private var displayText = "Hello, world!"
     @State private var errorText = ""
     @State private var showError = false
-    var body: some SwiftUI.View {
-        GeometryReader { geometry in
-            SwiftUI.ZStack {
-                SwiftUI.VStack {
-                    SwiftUI.Image(systemName: "globe")
-                        .imageScale(.large)
-                        .foregroundStyle(.tint)
-                    SwiftUI.Text(displayText)
-                    SwiftUI.Button("Bye") {
-                        var mirror: String = "";
-                        var error: String = "";
-                        if mirrorText(input: displayText, output: &mirror, error: &error) == 0 {
-                            displayText = mirror
-                        } else {
-                            errorText = error
-                            withAnimation { showError = true }
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                                withAnimation { showError = false }
-                            }
-                        }
-                    }
-                    .padding()
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .onTapGesture { withAnimation { showError = false } }
-                SwiftUI.VStack(alignment: .center) {
-                    if showError {
-                        Text("💣 \(errorText)")
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(Color.red.opacity(0.4))
-                            .clipShape(RoundedBottomRectangle(cornerRadius: 5))
-                            .transition(.move(edge: .top))
-                            .zIndex(1)
-                            .padding(.top, showError ? -geometry.size.height / 2 : -100)
-                            .animation(.easeInOut, value: showError)
-                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text(displayText)
+                Button("Bye") {
+                    let r = mirrorText(input: displayText)
+                    if r.err == 0 {
+                        displayText = r.output
+                    } else {
+                        errorText = r.error
+                        withAnimation { showError = true }
                     }
                 }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding()
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .onTapGesture { withAnimation { showError = false } }
+            ToastView(message: errorText, isError: true, isVisible: $showError)
         }
     }
 }
-
-struct RoundedBottomRectangle: Shape {
-
-    var cornerRadius: CGFloat
-    
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        let topLeft = rect.origin
-        let topRight = CGPoint(x: rect.maxX, y: rect.origin.y)
-        let bottomRight = CGPoint(x: rect.maxX, y: rect.maxY)
-        let bottomLeft = CGPoint(x: rect.origin.x, y: rect.maxY)
-        path.move(to: topLeft)
-        path.addLine(to: topRight)
-        path.addLine(to: bottomRight)
-        path.addArc(center: CGPoint(x: bottomRight.x - cornerRadius, y: bottomRight.y - cornerRadius), radius: cornerRadius, startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 90), clockwise: false)
-        path.addLine(to: CGPoint(x: bottomLeft.x + cornerRadius, y: bottomLeft.y))
-        path.addArc(center: CGPoint(x: bottomLeft.x + cornerRadius, y: bottomLeft.y - cornerRadius), radius: cornerRadius, startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 180), clockwise: false)
-        path.addLine(to: topLeft)
-        return path
-    }
-}
-
 
 struct ContentView_Previews: SwiftUI.PreviewProvider {
     static var previews: some SwiftUI.View {
--- YLIP/cmodule.swift
+++ YLIP/cmodule.swift
@@ -6,18 +6,3 @@ public func mirror(_ input: UnsafePointer<UInt8>?,
                    _ output: UnsafeMutablePointer<UInt8>?,
                    _ output_bytes: UnsafeMutablePointer<Int64>?) -> Int32
 
-func mirrorText(input: String, output: inout String, error: inout String) -> Int32 {
-    var r: Int32 = 0
-    var output_cstr = Array<UInt8>(repeating: 0, count: input.utf8.count + 1)
-    var output_bytes = Int64(input.utf8.count + 1)
-    input.withCString { input_cstr in
-        r = mirror(UnsafeRawPointer(input_cstr).assumingMemoryBound(to: UInt8.self),
-                   Int64(input.utf8.count), &output_cstr, &output_bytes)
-        if r == 0 {
-            output = String(cString: output_cstr)
-        } else {
-            error = String(cString: strerror(r))
-        }
-    }
-    return r
-}
